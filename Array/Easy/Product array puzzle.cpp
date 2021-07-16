9342b129175d01a2de565350c95531c6cb9b38f3
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2021-07-16 17:32:11 +0530

    Create Product array puzzle.cpp

---
+++ Array/Easy/Product array puzzle.cpp
@@ -0,0 +1,81 @@
+/*
+
+platform: GFG
+
+Product array puzzle 
+Easy Accuracy: 47.32% Submissions: 41762 Points: 2
+Given an array nums[] of size n, construct a Product Array P (of same size n) such that P[i] is equal to the product of all the elements of nums except nums[i].
+
+ 
+
+Example 1:
+
+Input:
+n = 5
+nums[] = {10, 3, 5, 6, 2}
+Output:
+180 600 360 300 900
+Explanation: 
+For i=0, P[i] = 3*5*6*2 = 180.
+For i=1, P[i] = 10*5*6*2 = 600.
+For i=2, P[i] = 10*3*6*2 = 360.
+For i=3, P[i] = 10*3*5*2 = 300.
+For i=4, P[i] = 10*3*5*6 = 900.
+Example 2:
+
+Input:
+n = 2
+nums[] = {12,0}
+Output:
+0 12
+
+Your Task:
+You do not have to read input. Your task is to complete the function productExceptSelf() that takes array nums[] and n as input parameters and returns a list of n integers denoting the product array P. If the array has only one element the returned list should should contains one value i.e {1}
+Note: Try to solve this problem without using the division operation.
+ 
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+ 
+
+Constraints:
+1 <= n <= 1000
+0 <= numsi <= 200
+Array may contain duplicates.
+
+*/
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+        //code here    
+       // Allocate memory for product array andinitialise it by 1
+        vector<long long int> vec(n, 1ll);
+        if (n == 1) 
+        {
+            return vec;
+        }
+        
+        // In this loop temp will store the product of values on the left side of nums[i]
+        long long temp = 1ll;
+        for(int i = 0;i < n;i++) 
+        {
+            vec[i] *= temp;
+            temp *= nums[i];
+        }
+        
+        // In this loop temp will store the product of values on the right side of nums[i]
+        temp = 1ll;
+        for(int i = n - 1;i >= 0;i--)
+        {
+            vec[i] *= temp;
+            temp *= nums[i];
+        }
+        
+        
+        return vec; // return the expected vector
+    }
+};

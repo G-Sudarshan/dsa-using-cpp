c209bb6b5d2c1dd281fd5fcdc989c25eae65d039
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2021-07-27 06:31:09 +0530

    Create Key Pair.cpp

---
+++ Hashing/Easy/Key Pair.cpp
@@ -0,0 +1,52 @@
+/*
+
+platform: GFG
+
+Key Pair 
+Easy Accuracy: 49.96% Submissions: 22007 Points: 2
+Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
+
+Example 1:
+
+Input:
+N = 6, X = 16
+Arr[] = {1, 4, 45, 6, 10, 8}
+Output: Yes
+Explanation: Arr[3] + Arr[4] = 6 + 10 = 16
+Example 2:
+
+Input:
+N = 5, X = 10
+Arr[] = {1, 2, 4, 3, 6}
+Output: Yes
+Explanation: Arr[2] + Arr[4] = 4 + 6 = 10
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function hasArrayTwoCandidates() which takes the array of integers arr, n and x as parameters and returns boolean denoting the answer.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ Arr[i] ≤ 105
+
+*/
+
+class Solution{
+public:
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    unordered_map<int, int> hash; // declare hashmap
+    	for (int i = 0; i < n; i++) {
+    		int numberToFind = x - arr[i]; // number to be find
+                //if numberToFind is found in map, return them
+    		if (hash.find(numberToFind) != hash.end()) {
+    		    return true;
+    		}
+                //number was not found. Put it in the map.
+    		hash[arr[i]] = i;
+    	}
+    	return false;
+	}
+};
